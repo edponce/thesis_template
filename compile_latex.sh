3ebe3efde0f71f02f48afc4a081edf972af7b96e
Eduardo Ponce <edponce00@hotmail.com>
2016-07-25 04:40:50 -0500

    update to compile script

--- compile_latex.sh
+++ compile_latex.sh
@@ -1,17 +1,18 @@
 #! /usr/bin/env bash 
 
 # Usage information
-if [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
-    echo
-    echo "compile_latex [-h|--help] [LaTeX file] [output directory]"
-    echo
-    echo "Comments:"
-    echo " 1. If LaTeX file is not provided, current directory is searched for a file."
-    echo " 2. An explicit LaTeX file has to be provided before specifying output directory."
-    echo " 3. Default output directory is 'build'."
-    echo
-    exit
-fi
+case $1 in 
+    -h | --h*)
+    cat <<\EOF
+Usage: compile_latex [--help] [LaTeX file [output directory]]
+
+If LaTeX file is not provided, current directory is searched for a file.
+
+Default output directory is 'build'.
+EOF
+    exit 0
+    ;;
+esac
 
 # Validate TeX file
 TEXFILE="$1"
@@ -22,12 +23,12 @@ if [ -z "$1" ]; then
         echo "Warning: automatic TeX file found, '$TEXFILE'."
     else
         echo "Error: too many TeX files found, '$TEXFILE'."
-        exit 
+        exit 1
     fi
 fi
 if [ ! -e "$TEXFILE" ]; then
     echo "Error: TeX file does not exists, '$TEXFILE'."
-    exit
+    exit 1
 fi
 
 # Validate output directory
@@ -45,10 +46,12 @@ BASEFILE="${TEXFILE%.*}"
 # Define commands
 PDFLATEX_CMD="pdflatex -synctex=1 -interaction=nonstopmode -output-dir=$OUTDIR $TEXFILE"
 BIBTEX_CMD="bibtex $OUTDIR/$BASEFILE"
+PDFVIEW_CMD="evince $OUTDIR/$BASEFILE.pdf"
 
 # Run commands
 $PDFLATEX_CMD
 $BIBTEX_CMD
 $PDFLATEX_CMD
 $PDFLATEX_CMD
+$PDFVIEW_CMD &
 
